fa020c6e791bf1d9e9ed55c014fab2bdc23fd549
MoyukhShabon <moyukh.work@gmail.com>
2025-10-11 00:59:59 +0800

    added scripts to get bam data from seqc2 ncbi ftp

--- data/bam/get.sh
+++ data/bam/get.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # View files to download
 # https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/seqc/Somatic_Mutation_WG
 
 # Download files
-wget -c -r -nH --cut-dirs=4 --no-parent --execute robots=off "ftp://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/seqc/Somatic_Mutation_WG/data"
+# wget -c -r -nH --cut-dirs=4 --no-parent --execute robots=off "ftp://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/seqc/Somatic_Mutation_WG/data"
+
+bash get_wgs.sh
+bash get_wes.sh
+bash get_ffg.sh
+bash get_ffx.sh
+bash get_lbp.sh
+bash get_ams.sh
---
+++ data/bam/get_ams.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download files
+wget -c -r -nH --cut-dirs=4 --no-parent --execute robots=off "ftp://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/seqc/Somatic_Mutation_WG/data/AmpliSeq_bams"
+mv AmpliSeq_bams AMS
