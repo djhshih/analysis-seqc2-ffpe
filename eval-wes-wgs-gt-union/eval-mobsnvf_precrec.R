b5134fc588bdb1050fadace3a67f8932c3d2d252
MoyukhShabon <moyukh.work@gmail.com>
2025-11-25 21:45:05 +0800

    added eval scripts for eval with combined ground truth (WGS + WES). | Draft

---
+++ eval-wes-wgs-gt-union/eval-mobsnvf_precrec.R
@@ -0,0 +1,341 @@
+#!/usr/bin/env bash
+library(io)
+library(precrec)
+
+source("../common-ffpe-snvf/R/eval.R")
+
+#####################################################################################
+
+# Function for performing per-sample evaluation using a unified ground truth derived
+# from the union of all Fresh Frozen (FF) samples.
+# @param ffpe_snvf_dir (string) | Directory containing the model's SNV filtering results for a specific dataset.
+# @param model_name (string) | Name of the model being evaluated, used in file paths.
+# @param ground_truth_variants (data.frame) | A data frame of all true variants, typically created by snv_union().
+# @param outdir_root (string) | Root output directory for writing evaluation results.
+
+process_samples <- function(ffpe_snvf_dir, model_name, ground_truth_variants, outdir_root) {
+	message("Processing samples with a unified ground truth:")
+
+	# Construct the search path to find all of the model's result files
+	search_path <- file.path(ffpe_snvf_dir, model_name, "*", sprintf("*.%s.snv", model_name))
+	model_result_paths <- Sys.glob(search_path)
+
+	if (length(model_result_paths) == 0) {
+		message(sprintf("Warning: No result files found for model '%s' at search path: %s", model_name, search_path))
+		return()
+	}
+
+	message(sprintf("Found %d result files to process.", length(model_result_paths)))
+
+	# Evaluate each sample against the unified ground truth
+	for (path in model_result_paths) {
+		# Extract sample name from the file path. Assumes format 'sample_name.model_name.snv'
+		sample_name <- unlist(strsplit(basename(path), "\\."))[1]
+		message(sprintf("	%s: %s", sample_name, path))
+
+		# Read and preprocess the data, annotating with truth labels
+		d <- read.delim(path)
+		d <- preprocess_mobsnvf(d, ground_truth_variants)
+
+		# Check if truth labels are not exclusively TRUE or FALSE.
+		# Cases like these are skipped as evaluation is not supported by precrec.
+		if (nrow(d[d$truth, ]) == 0 | nrow(d[!d$truth, ]) == 0) {
+			message(sprintf("		truth labels consists of only one class for %s. Skipping evaluation for this sample.", sample_name))
+			next
+		}
+
+		# Evaluate the filter's performance for the individual sample
+		res <- evaluate_filter(d, model_name, sample_name)
+
+		# Write per-sample results (scores/truths table and evaluation metrics)
+		write_sample_eval(d, res, outdir_root, sample_name, model_name)
+	}
+}
+
+
+# Function for combining the ground truth annotated SNV score tables from a directory structure.
+# @param score_truth_outdir (string) | Directory where the per-sample ground truth annotated scores were saved.
+# @param model_name (string) | Name of model being evaluated. Used to find the correct files.
+
+combine_snv_score_truth <- function(score_truth_outdir, model_name) {
+	message("Combining all per-sample ground truth SNV score tables into one")
+
+	# Construct the search path for the score/truth files written by write_sample_eval
+	search_pattern <- sprintf("*_%s-scores_truths.tsv", model_name)
+	score_truth_paths <- Sys.glob(file.path(score_truth_outdir, "*", search_pattern))
+
+	if (length(score_truth_paths) == 0) {
+		message(sprintf("Warning: No score/truth files found in '%s' for model '%s'", score_truth_outdir, model_name))
+		return(data.frame()) # Return empty data frame if no files are found
+	}
+    
+	message(sprintf("Found %d score/truth files to combine.", length(score_truth_paths)))
+
+	# Read each file, add the sample name, and combine into a single data frame
+	all_score_truth <- do.call(
+		rbind,
+		lapply(score_truth_paths, function(path) {
+			# Extract sample name from the file name
+			file_basename <- basename(path)
+			sample_name <- gsub(sprintf("_%s-scores_truths.tsv", model_name), "", file_basename)
+			
+			message(sprintf("	Reading data for sample %s", sample_name))
+			
+			df <- read.delim(path)
+			df$sample_name <- sample_name
+			df
+		})
+	)
+	
+	return(all_score_truth)
+}
+
+evaluate_dataset <- function(
+	dataset_id,
+	ff_dataset_id,
+	vcf_dir,
+	ffpe_snvf_dir,
+	outdir_root,
+	model_name,
+	agg_name
+){
+
+	message(sprintf("Dataset: %s", dataset_id))
+
+	# Directory for the FFPE SNV filtering results
+	ffpe_snvf_dir <- file.path(ffpe_snvf_dir, dataset_id)
+	# Root output directory
+	outdir_root <- file.path(outdir_root, dataset_id)
+	# Specific output directory for combined scores and truths
+	score_truth_outdir <- file.path(outdir_root, "model-scores_truths")
+	# Specific output directory for evaluation plots and metrics
+	eval_outdir <- file.path(outdir_root, "roc-prc-auc/precrec")
+
+
+	# Create a unified ground truth by taking the union of all variants from Fresh Frozen samples
+	message("Generating unified ground truth from all Fresh Frozen samples (WES + WGS dataset)...")
+	ff_paths <- c(Sys.glob(file.path(vcf_dir, "WGS/*/*_T_*.vcf")), Sys.glob(file.path(vcf_dir, "WES/*/*_T_*.vcf")))
+
+	gt_name <- "WES_WGS_ff-variants_set.tsv"
+	if (file.exists(gt_name)){
+		message("Using pregenerated ground truth")
+		ff_variants <- qread(gt_name)
+	} else {
+		ff_variants <- snv_union(ff_paths)
+		message("Ground truth generated.")
+		qwrite(ff_variants, "WES_WGS_ff-variants_set.tsv")
+	}
+
+	# Perform per-sample evaluation
+	process_samples(
+		ffpe_snvf_dir = ffpe_snvf_dir, 
+		model_name = model_name, 
+		ground_truth_variants = ff_variants, 
+		outdir_root = outdir_root
+	)
+
+	# Combine the per-sample score/truth tables into a single data frame
+	all_score_truth <- combine_snv_score_truth(
+		score_truth_outdir = score_truth_outdir, 
+		model_name = model_name
+	)
+
+	# Proceed with overall evaluation if combined data is available
+	if (nrow(all_score_truth) > 0) {
+		message("Performing overall evaluation...")
+		## Evaluate overall result
+		overall_res <- evaluate_filter(all_score_truth, model_name, agg_name)
+
+		## Write overall result to disk
+		write_overall_eval(all_score_truth, overall_res, score_truth_outdir, eval_outdir, agg_name, model_name)
+		message("Overall evaluation complete.")
+	} else {
+		message("Skipping overall evaluation as no combined score/truth data was generated.")
+	}
+	
+}
+
+#################################################################################
+
+model_name <- "mobsnvf"
+message(sprintf("Evaluating %s: ", model_name))
+
+# ################################# SEQC2 FFX w/ Matched Normal ########################################
+
+# # Filtered FFX dataset vs Unfiltered WES VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFX",
+# 	ff_dataset_id = "WES",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_filtermutectcalls_obmm_unfiltered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wes-samples"
+# )
+
+
+# # Filtered FFX dataset vs Filtered WES VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFX",
+# 	ff_dataset_id = "WES",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wes-samples"
+# )
+
+# # Filtered FFX dataset vs Filtered WES VCF, DP >= 10
+# evaluate_dataset(
+# 	dataset_id = "FFX",
+# 	ff_dataset_id = "WES",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-dp-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-dp-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-dp-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wes-samples"
+# )
+
+# # Filtered FFX dataset vs Filtered WES VCF; DP>=20
+# evaluate_dataset(
+# 	dataset_id = "FFX",
+# 	ff_dataset_id = "WES",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-dp20-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-dp20-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-dp20-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wes-samples"
+# )
+
+# Filtered FFX dataset vs Filtered WES VCF; MICR removed
+evaluate_dataset(
+	dataset_id = "FFX",
+	ff_dataset_id = "WES",
+	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-filtered",
+	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-filtered_micr",
+	outdir_root = "mutect2-matched-normal_pass-orientation-filtered_micr.vs.filtered-ff",
+	model_name = model_name,
+	agg_name = "all-ffpe-wes-samples"
+)
+
+# Filtered FFX dataset vs Filtered WES VCF, DP >= 10; MICR removed
+evaluate_dataset(
+	dataset_id = "FFX",
+	ff_dataset_id = "WES",
+	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-dp-filtered",
+	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-dp-filtered_micr",
+	outdir_root = "mutect2-matched-normal_pass-orientation-dp-filtered_micr.vs.filtered-ff",
+	model_name = model_name,
+	agg_name = "all-ffpe-wes-samples"
+)
+
+# Filtered FFX dataset vs Filtered WES VCF; DP>=20; MICR removed
+evaluate_dataset(
+	dataset_id = "FFX",
+	ff_dataset_id = "WES",
+	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-dp20-filtered",
+	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-dp20-filtered_micr",
+	outdir_root = "mutect2-matched-normal_pass-orientation-dp20-filtered_micr.vs.filtered-ff",
+	model_name = model_name,
+	agg_name = "all-ffpe-wes-samples"
+)
+
+# ################################# SEQC2 FFG w/ Matched Normal ########################################
+
+# # FFG dataset vs Unfiltered WGS VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFG",
+# 	ff_dataset_id = "WGS",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_filtermutectcalls_obmm_unfiltered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wgs-samples"
+# )
+
+# # FFG dataset vs Filtered WGS VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFG",
+# 	ff_dataset_id = "WGS",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wgs-samples"
+# )
+
+
+# # FFG dataset vs Filtered WGS VCF, DP>=10
+# evaluate_dataset(
+# 	dataset_id = "FFG",
+# 	ff_dataset_id = "WGS",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-dp-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-dp-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-dp-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wgs-samples"
+# )
+
+# # FFG dataset vs Filtered WGS VCF, DP>=20
+# evaluate_dataset(
+# 	dataset_id = "FFG",
+# 	ff_dataset_id = "WGS",
+# 	vcf_dir = "../vcf/mutect2-matched-normal_pass-orientation-dp20-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-matched-normal_pass-orientation-dp20-filtered",
+# 	outdir_root = "mutect2-matched-normal_pass-orientation-dp20-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wgs-samples"
+# )
+
+
+# ################################# SEQC2 FFX Tumor Only ########################################
+# message("Tumor-Only VCF evaluation")
+
+
+# # Filtered FFX dataset vs Filtered WES VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFX",
+# 	ff_dataset_id = "WES",
+# 	vcf_dir = "../vcf/mutect2-tumor-only_pass-orientation-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-tumor-only_pass-orientation-filtered",
+# 	outdir_root = "mutect2-tumor-only_pass-orientation-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wes-samples"
+# )
+
+
+# # Filtered FFX dataset vs Unfiltered WES VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFX",
+# 	ff_dataset_id = "WES",
+# 	vcf_dir = "../vcf/mutect2-tumor-only_filtermutectcalls_obmm_unfiltered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-tumor-only_pass-orientation-filtered",
+# 	outdir_root = "mutect2-tumor-only_pass-orientation-filtered.vs.unfiltered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wes-samples"
+# )
+
+################################# SEQC2 FFG Tumor Only ########################################
+
+# # FFG dataset vs Unfiltered WGS VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFG",
+# 	ff_dataset_id = "WGS",
+# 	vcf_dir = "../vcf/mutect2-tumor-only_filtermutectcalls_obmm_unfiltered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-tumor-only_pass-orientation-filtered",
+# 	outdir_root = "mutect2-tumor-only_pass-orientation-filtered.vs.unfiltered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wgs-samples"
+# )
+
+# # FFG dataset vs Filtered WGS VCF 
+# evaluate_dataset(
+# 	dataset_id = "FFG",
+# 	ff_dataset_id = "WGS",
+# 	vcf_dir = "../vcf/mutect2-tumor-only_pass-orientation-filtered",
+# 	ffpe_snvf_dir = "../ffpe-snvf/mutect2-tumor-only_pass-orientation-filtered",
+# 	outdir_root = "mutect2-tumor-only_pass-orientation-filtered.vs.filtered-ff",
+# 	model_name = model_name,
+# 	agg_name = "all-ffpe-wgs-samples"
+# )
+
---
+++ eval-wes-wgs-gt-union/make-plots.R
@@ -0,0 +1,83 @@
+## This script is used to make ROC and PRC plots from the evaluation results aggregated using combine_results.R
+source("../common-ffpe-snvf/R/plot.R")
+
+dset_dirs <- c(
+	"mutect2-matched-normal_pass-orientation-filtered_micr.vs.filtered-ff/FFX",
+	"mutect2-matched-normal_pass-orientation-dp-filtered_micr.vs.filtered-ff/FFX",
+	"mutect2-matched-normal_pass-orientation-dp20-filtered_micr.vs.filtered-ff/FFX"
+	# "mutect2-matched-normal_pass-orientation-dp-filtered.vs.filtered-ff/FFG",
+	# "mutect2-matched-normal_pass-orientation-dp-filtered.vs.filtered-ff/FFX",
+	# "mutect2-matched-normal_pass-orientation-dp20-filtered.vs.filtered-ff/FFG",
+	# "mutect2-matched-normal_pass-orientation-dp20-filtered.vs.filtered-ff/FFX",
+	# "mutect2-tumor-only_pass-orientation-filtered.vs.filtered-ff/FFX",
+	# "mutect2-tumor-only_pass-orientation-filtered.vs.filtered-ff/FFG",
+	# "mutect2-tumor-only_pass-orientation-filtered.vs.unfiltered-ff/FFX",
+	# "mutect2-tumor-only_pass-orientation-filtered.vs.unfiltered-ff/FFG",
+	# "mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff/FFX",
+	# "mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff/FFG",
+	# "mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff/FFX",
+	# "mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff/FFG"
+)
+
+dset_author <- c(
+	"FFX" = "SEQC-II WES FFPE",
+	"FFG" =  "SEQC-II WGS FFPE"
+)
+
+models <- c(
+	"mobsnvf" = "MOBSNVF",
+	"vafsnvf" = "VAFSNVF",
+	"gatk-obmm" = "GATK-OBMM",
+	"sobdetector" = "SOBDetector",
+	"microsec" = "MicroSEC",
+	"ideafix-rf" = "Ideafix-RF",
+	"ideafix-xgboost" = "Ideafix-XGBoost"
+)
+
+for (dir in dset_dirs){
+
+	tokens <- unlist(strsplit(dir, "/"))
+	dset <- tokens[length(tokens)]
+	message(sprintf("Processing dataset: %s", dset))
+
+	## Set output directory
+	outdir_root <- file.path(dir, "plots_publication")
+
+	## Make a vector with paths to all the precrec eval objects
+	roc_coord_paths <- sort(list.files(file.path(dir, "roc-prc-auc/precrec"), pattern = "all-models_roc_coordinates.tsv", recursive = TRUE, full.names = TRUE))
+	prc_coord_paths <- sort(list.files(file.path(dir, "roc-prc-auc/precrec"), pattern = "all-models_prc_coordinates.tsv", recursive = TRUE, full.names = TRUE))
+
+	## Plot display size parameters for debugging
+	# options(repr.plot.width = 6, repr.plot.height = 5)
+
+	## Create plots for each of the evaluated samples
+	message("Creating ROC PRC plot for:")
+
+	outdir <- glue("{outdir_root}/roc_prc_plots")
+	dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+
+	for (i in seq_len(length(roc_coord_paths))) {
+
+		sample_name <- match_return_sample_name(roc_coord_paths[i], prc_coord_paths[i])
+		message(glue("\t {i}. {sample_name}"))
+
+		# ## Read in the variant set to count number and annotate the number of SNVs in the plot
+		# eval_snv_set <- qread(glue("PRJEB8754/vcf_pass-orient-pos-sb_ad_filtered/model-scores_truths/{sample_name}/{sample_name}_model-scores_truths.tsv"))
+		# snv_count <- nrow(eval_snv_set)
+
+		roc_coord <- qread(roc_coord_paths[i])
+		roc_coord$model <- ifelse(roc_coord$model %in% names(models), models[roc_coord$model], roc_coord$model)
+
+		prc_coord <- qread(prc_coord_paths[i])
+		prc_coord$model <- ifelse(prc_coord$model %in% names(models), models[prc_coord$model], prc_coord$model)
+
+		plots <- make_roc_prc_plot(roc_coord, prc_coord, title = dset_author[dset], subtitle = NULL, caption = NULL, text_scale=2, line_width = 1.5, legend_rows = 2, individual_plots = TRUE, legend_scale = 0.8)
+
+		qdraw(plots$roc_prc, glue("{outdir}/{sample_name}_roc_prc_plot.pdf"), width = 8, height = 6)
+		dir.create(glue("{outdir}/roc"), recursive = TRUE, showWarnings = FALSE)
+		qdraw(plots$roc, glue("{outdir}/roc/{sample_name}_roc_plot.pdf"), width = 5, height = 5)
+		dir.create(glue("{outdir}/prc"), recursive = TRUE, showWarnings = FALSE)
+		qdraw(plots$prc, glue("{outdir}/prc/{sample_name}_prc_plot.pdf"), width = 5, height = 5)
+
+	}
+}
