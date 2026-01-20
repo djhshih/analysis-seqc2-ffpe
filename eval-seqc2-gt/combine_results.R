3513ee978941ba0042ad3fe26a5eda245bb7d162
MoyukhShabon <moyukh.work@gmail.com>
2026-01-21 02:27:50 +0800

    added new evaluation scripts

---
+++ eval-seqc2-gt/combine_results.R
@@ -0,0 +1,87 @@
+## This script is used to combine the results after evaluation of the ffpe filtering models
+library(io)
+
+## Dataset specific
+eval_dirs <- c(
+	"mutect2-matched-normal_hc-target_pass-orientation-dp-blacklist-filtered/FFG",
+	"mutect2-matched-normal_exome-hc-target_pass-orientation-dp-blacklist-filtered/FFX"
+)
+
+for (eval_dir in eval_dirs){
+
+	eval_dir <- file.path(eval_dir, "roc-prc-auc/precrec")
+
+	## List name of models that were evaluated. 
+	models <- c("all-models", "mobsnvf", "vafsnvf", "sobdetector", "gatk-obmm", "microsec", "ideafix-rf", "ideafix-xgboost", "ffpolish")
+
+	## Combine the AUC table for each sample and overall eval i.e all sample, liver samples, colon samples
+	message("Combining AUCs")
+	auc_paths <- list.files(eval_dir, pattern = "_auc_table.tsv", recursive = TRUE, full.names = TRUE)
+	auc_paths <- auc_paths[!grepl("combined", auc_paths)]
+
+	auc <- do.call(
+		rbind,
+		lapply(
+			auc_paths, 
+			read.delim
+		)
+	)
+
+	auc <- auc[order(auc$sample_name, auc$model),]
+
+	qwrite(auc, file.path(eval_dir, "combined_auc_table.tsv"))
+
+
+	## List samples based on their path
+	roc_path <- list.files(eval_dir, pattern = "_roc_coordinates.tsv", recursive = TRUE, full.names = TRUE)
+	to_remove <- c(paste("", models, sep = "_"), '_roc_coordinates.tsv','_prc_coordinates.tsv')
+
+	samples <- unique(sapply(
+		roc_path, 
+		function(i) {
+			s <- basename(i)
+			for (p in to_remove) {
+				s <- gsub(p, "", s, fixed = TRUE)
+			}
+			s
+		}, 
+		USE.NAMES = FALSE
+	))
+
+	# Combine ROC and PRC results for all models within each sample
+	message("Combining ROC and PRC results for the evaluated model within each sample:")
+	for (sample in samples){
+
+		message(sample)
+		paths <- list.files(eval_dir, pattern = sample, recursive = TRUE, full.names = TRUE)
+
+		## Combine ROC
+		roc_paths <- paths[grepl("roc_coordinates.tsv", paths)]
+		roc_paths <- roc_paths[!grepl("all-models_roc_coordinates.tsv", roc_paths)]
+		
+		roc <- do.call(
+			rbind,
+			lapply(roc_paths, read.delim)
+		)
+
+		dir <- dirname(roc_paths[1])
+		qwrite(roc, file.path(dir, sprintf("%s_all-models_roc_coordinates.tsv", sample)))
+
+
+		# Combine PRC
+		prc_paths <- paths[grepl("prc_coordinates.tsv", paths)]
+		prc_paths <- prc_paths[!grepl("all-models_prc_coordinates.tsv", prc_paths)]
+		
+		prc <- do.call(
+			rbind,
+			lapply(prc_paths, read.delim)
+		)
+
+		dir <- dirname(prc_paths[1])
+		qwrite(prc, file.path(dir, sprintf("%s_all-models_prc_coordinates.tsv", sample)))
+
+	}
+
+	message("Done.")
+
+}
---
+++ eval-seqc2-gt/make-plots.R
@@ -0,0 +1,70 @@
+## This script is used to make ROC and PRC plots from the evaluation results aggregated using combine_results.R
+source("../common-ffpe-snvf/R/plot.R")
+
+dset_dirs <- c(
+	"mutect2-matched-normal_hc-target_pass-orientation-dp-blacklist-filtered/FFG",
+	"mutect2-matched-normal_exome-hc-target_pass-orientation-dp-blacklist-filtered/FFX"
+)
+
+dset_author <- c(
+	"FFX" = "SEQC-II WES FFPE",
+	"FFG" =  "SEQC-II WGS FFPE"
+)
+
+models <- c(
+	"mobsnvf" = "MOBSNVF",
+	"vafsnvf" = "VAFSNVF",
+	"gatk-obmm" = "GATK-OBMM",
+	"sobdetector" = "SOBDetector",
+	"microsec" = "MicroSEC",
+	"ideafix-rf" = "Ideafix-RF",
+	"ideafix-xgboost" = "Ideafix",
+	"ffpolish" = "FFPolish"
+)
+
+for (dir in dset_dirs){
+
+	dset <- basename(dir)
+	message(sprintf("Processing: %s", dir))
+
+	## Set output directory
+	outdir_root <- file.path(dir, "plots")
+
+	## Make a vector with paths to all the precrec eval objects
+	roc_coord_paths <- sort(list.files(file.path(dir, "roc-prc-auc/precrec"), pattern = "all-models_roc_coordinates.tsv", recursive = TRUE, full.names = TRUE))
+	prc_coord_paths <- sort(list.files(file.path(dir, "roc-prc-auc/precrec"), pattern = "all-models_prc_coordinates.tsv", recursive = TRUE, full.names = TRUE))
+
+	## Plot display size parameters for debugging
+	# options(repr.plot.width = 6, repr.plot.height = 5)
+
+	## Create plots for each of the evaluated samples
+	message("Creating ROC PRC plot for:")
+
+	outdir <- glue("{outdir_root}/roc_prc_plots")
+	dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+
+	for (i in seq_len(length(roc_coord_paths))) {
+
+		sample_name <- match_return_sample_name(roc_coord_paths[i], prc_coord_paths[i])
+		message(glue("\t {i}. {sample_name}"))
+
+		# ## Read in the variant set to count number and annotate the number of SNVs in the plot
+		# eval_snv_set <- qread(glue("PRJEB8754/vcf_pass-orient-pos-sb_ad_filtered/model-scores_truths/{sample_name}/{sample_name}_model-scores_truths.tsv"))
+		# snv_count <- nrow(eval_snv_set)
+
+		roc_coord <- qread(roc_coord_paths[i])
+		roc_coord$model <- ifelse(roc_coord$model %in% names(models), models[roc_coord$model], roc_coord$model)
+
+		prc_coord <- qread(prc_coord_paths[i])
+		prc_coord$model <- ifelse(prc_coord$model %in% names(models), models[prc_coord$model], prc_coord$model)
+
+		plots <- make_roc_prc_plot(roc_coord, prc_coord, title = dset_author[dset], subtitle = NULL, caption = NULL, text_scale=2, line_width = 1.5, legend_rows = 3, individual_plots = TRUE, legend_scale = 0.8)
+
+		qdraw(plots$roc_prc, glue("{outdir}/{sample_name}_roc_prc_plot.pdf"), width = 8, height = 6)
+		dir.create(glue("{outdir}/roc"), recursive = TRUE, showWarnings = FALSE)
+		qdraw(plots$roc, glue("{outdir}/roc/{sample_name}_roc_plot.pdf"), width = 5, height = 6)
+		dir.create(glue("{outdir}/prc"), recursive = TRUE, showWarnings = FALSE)
+		qdraw(plots$prc, glue("{outdir}/prc/{sample_name}_prc_plot.pdf"), width = 5, height = 6)
+
+	}
+}
